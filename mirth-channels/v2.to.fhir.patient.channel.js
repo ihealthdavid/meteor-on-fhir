5e60989bcf17287c16bae9a9efaf674449880e71
ihealthdavid <ihealthdavid@users.noreply.github.com>
2017-08-16 00:21:10 -0700

    v2.to.fhir.patient.channel: gender 'female' and family not array
    Small changes to match latest Patient schema:
    - part of HL7 v2 to FHIR tutorial https://medium.com/@awatson1978/hl7-v2-to-fhir-interface-mapping-f83c6ecf6bee
    - gender 'female' https://github.com/clinical-meteor/hl7-resource-patient/blob/c517a97c6d5aa48aaaaee8aed9e7f8ec3c4268b4/lib/Patients.js#L93
    - family not array (STU3) https://github.com/clinical-meteor/hl7-resource-patient/blob/c517a97c6d5aa48aaaaee8aed9e7f8ec3c4268b4/lib/Patients.js#L351

--- mirth-channels/v2.to.fhir.patient.channel.js
+++ mirth-channels/v2.to.fhir.patient.channel.js
@@ -3,7 +3,7 @@ var date = msg['PID']['PID.7']['PID.7.1'].toString();
 var Patient = {};
 
 Patient.resourceType = "Patient";
-Patient.gender = msg['PID']['PID.8']['PID.8.1'].toString();
+Patient.gender = msg['PID']['PID.8']['PID.8.1'].toString() === 'F' ? 'female' : 'male';
 Patient.birthDate = date.substr(0,4) + "-" + date.substr(4,2) + "-" + date.substr(6,2);
 Patient.identifier = [{
      "use": "usual",
@@ -21,7 +21,7 @@ Patient.identifier = [{
 
 
 var humanName = {
-	family: [msg['PID']['PID.5']['PID.5.1'].toString()],
+	family: msg['PID']['PID.5']['PID.5.1'].toString(),
 	given: [msg['PID']['PID.5']['PID.5.2'].toString()],
 	text: msg['PID']['PID.5']['PID.5.2'].toString() + ' ' + msg['PID']['PID.5']['PID.5.1'].toString()
 };
